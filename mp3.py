e892b3d5011cde31e6cddd406004ef748eb4af6c
Celal Yeşil <celalyesil@aof.anadolu.edu.tr>
2021-08-06 08:47:47 +0300

    first

---
+++ mp3.py
@@ -0,0 +1,19 @@
+import youtube_dl
+
+video_url = input("Youtube Link: ");
+
+video_bilgisi = youtube_dl.YoutubeDL().extract_info(
+	url = video_url,download=False)
+
+dosya_adi = f"{video_bilgisi['title']}.mp3"
+
+ayarlar={
+	'format':'bestaudio/best',
+	'keepvideo':True,
+	'outtmpl':dosya_adi,
+}
+
+with youtube_dl.YoutubeDL(ayarlar) as ydl:
+	ydl.download([video_bilgisi['webpage_url']])
+
+	print(f"indirme tamam... {dosya_adi}")
